d240c4db3f78281fc08899b9c7ec5f2bc27cecff
Clancastle <dennisungureanu0@gmail.com>
2023-08-07 06:44:18 -0500

    update
    worked on streaks, pretty hard id say, kinda. changed the way the database works.

--- importsl/data_functions.py
+++ importsl/data_functions.py
@@ -7,9 +7,8 @@ import matplotlib as mtp
 import re
 import time as tim
 from datetime import datetime
-from threading import Event
-from threading import Thread
-# from time_waste_checker.main import check_cmd
+
+date_yymmdd = datetime.today().strftime('%Y_%m_%d')
 
 
 cnn = mysql.connector.connect(port=3306, host='localhost', database='timeproject', password='kartoshka')
@@ -17,52 +16,55 @@ cnn = mysql.connector.connect(port=3306, host='localhost', database='timeproject
 cnn.connect()
 data = {}
 
+streak = 0
+
 cursor = cnn.cursor()
 def add_data():
-    try:
 
-        def call_inp():
-            print('\ntype \'x\' to close, when 24hs have been reached.\n')
-            print('Item name: (coding) (Example) \nTime spent: (05:30:00) format(hh/mm/ss)(example)\n')
-            var = input('item: ')
-            time = input('time spent: ')
-            return var, time
+    def call_inp():
+        print('\ntype \'x\' to close, when 24hs have been reached.\n')
+        print('Item name: (coding) (Example) \nTime spent: (05:30:00) format(hh/mm/ss)(example)\n')
+        var = input('item: ')
+        time = input('time spent: ')
+        return var, time
 
-        while True:
-            cnn.connect()
-            cursor = cnn.cursor()
-            var, time = call_inp()
+    while True:
+        cnn.connect()
+        cursor = cnn.cursor()
 
-            if (var == 'x') or (time == 'x'):
-                cursor.close()
+        var, timex = call_inp()
 
-                cnn.close()
-                return '/terminate'
+        if (var == 'x') or (timex == 'x'):
+            cursor.close()
+            cnn.close()
 
-            if (not re.search(r'[\d\W]', var)) and (re.match(r'(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)', time)):
-                print('Passed Validation')
-                data[var] = [time]
-                print('Creating..')
-                cursor.execute('insert into data(variable, time_spent, timestamp) values (%s, %s, %s)', (var, time, datetime.now()))
-                print('Created..')
-                cnn.commit()
-                continue
-            else:
-                cursor.close()
-                cnn.close()
-                return print('Try again with correct formatting.'), add_data()
+            return None  #'/terminate'
 
-        return data
-    except Exception as e:
-        cursor.close()
-        cnn.close()
-        return print(f'Error occurred: {e}\nCannot create..'), add_data()
+        if (not re.search(r'[\d\W]', var)) and (re.match(r'(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)', timex)):
+            print('Passed Validation')
+            data[var] = [timex]
+            print('Creating..')
+            tim.sleep(.3)
+            cursor.execute(f'create table if not exists {date_yymmdd}(variable varchar(255), time_spent time, timestamp timestamp)')
+            print('Created table')
+            cursor.execute(f'insert into {date_yymmdd} values (%s, %s, %s)', (var, timex, datetime.now()))
+            tim.sleep(.5)
 
+            print('Added to table..')
+
+            cnn.commit()
+            continue #want to continue
+        else:
+
+            return print('Try again with correct formatting.'), add_data()
+
+    cursor.close()
+    cnn.close()
+    return data
 
 def delete_data():
     def get_input():
         print('\nPlease read /info delete before using this to prevent unintentional data loss.')
-        print('e.x. \n> delete variable (or)\n> delete time')
         inp = input('\n>')
         return inp
 
@@ -74,7 +76,7 @@ def delete_data():
             if inp.split()[0] == 'recent':
                 print(f'Deleting last entry of: {inp.split()[1]}..')
                 cnn.connect()
-                query = 'delete from data where variable=(%s) order by timestamp desc limit 1;'
+                query = f'delete from {date_yymmdd} where variable=(%s) order by timestamp desc limit 1;'
                 cnn.cursor().execute(query, (inp.split()[1],))
                 print(f'Deleted')
                 cnn.commit()
@@ -85,7 +87,7 @@ def delete_data():
                 return None
             elif inp.split()[0] == 'all':
 
-                query = f'delete from data where variable=(%s)'
+                query = f'delete from {date_yymmdd} where variable=(%s)'
 
                 cnn.connect()
                 print(f'Deleting all {inp.split()[1]}..')
@@ -99,9 +101,10 @@ def delete_data():
                 return None
 
             elif inp.split()[0] == 'last':
+                print(date_yymmdd)
                 print('Deleting..')
                 cnn.connect()
-                cnn.cursor().execute('delete from data order by timestamp desc limit 1;')
+                cnn.cursor().execute(f'delete from {date_yymmdd} order by timestamp desc limit 1;')
                 cnn.commit()
                 print('Deleted..')
                 cnn.cursor().close()
@@ -114,68 +117,125 @@ def delete_data():
 
     except Exception as e:
         print(f'Error found: {e}\n Not Deleted..')
-
+#unneeded func
 
 def data_map():
-    print('used')
-
+    pass
 
 def render_bar(format='in'):
+    try:
+        cnn = mysql.connector.connect(port=3306, host='localhost', database='timeproject', password='kartoshka')
+
+        cnn.connect()
+        cursor = cnn.cursor()
+        str = []
+
+        cursor.execute(f'select * from {date_yymmdd};')
+        print('Collecting Data..')
+        tim.sleep(2)
+        data = cursor.fetchall()
+
+        for item, time, stamp in data:
+            'to convert hh:mm:ss format to hh.hhh'
+            time = time.total_seconds()
+            hours = int(time // 3600)
+            minutes = int((time % 3600) // 60)
+            seconds = time % 60
+            'finish'
+
+            hours_fraction = float(hours + minutes / 60 + seconds / 3600) #type = float
+
+            itemsx = (f'{item}', hours_fraction)# + , f'{stamp}
+            str.append(itemsx)
+
+        "converts into float for plotext" #i tried but somehow it didnt work
+        if format == 'out':
+            items, time_values = zip(*str)
+            plt.bar(items, time_values)
+            plt.xlabel("Item")
+            plt.ylabel("Time (hours)")
+            plt.title("Time Spent on Each Item")
+
+            plt.show()
+        else:
+            items, time_values = zip(*str)
+            plx.bar(items, time_values)
+            plx.xlabel("Item")
+            plx.ylabel("Time (hours)")
+            plx.title("Time Spent on Each Item")
+
+            plx.show()
+
+        cnn.cursor().close()
+        cnn.close()
+        return print('Bar chart rendered..')
+    except ValueError:
+        return print(f'Table {date_yymmdd} exists but has no values since all items were deleted.')
+
+def linear_regression():
+    pass
+
+
+def streaks(base=0):
+    streak = base
+    last = ''
+    data = clean_input()
+    now = datetime.today().strftime('%Y_%m_%d')
+    now = '2023_09_30'
+
+    i = 0
+    while i < len(data):
+        if now in data[i]: #means that it tests if time=now is in database, returns true
+            print('match: ',now, data[i])
+
+            yr, m, d = data[i][0].split('_')
+            yr, m, d = int(yr), int(m), int(d)
+
+            d-=1
+
+            print(d)
+            for x in range(len(data)):
+
+                i_yr, i_m, i_d = data[x][0].split('_')
+                i_yr, i_m, i_d = int(i_yr), int(i_m), int(i_m)
+                if (i_yr == yr) and (i_m == m) and (i_d == d):
+                    print('oo')
+                    streak+=1
+                    d-=1
+                    print(streak)
+                    continue
+
+
+
+
+        i+=1
+
+
+
+    return data
+
+def clean_input():
+    data = show_tables()
+
+    i = 0
+    while i < len(data): #tested, will be out of range if you dont do this. i remember to do this cause it counts from 0.
+        if len(data[i][0].split('_')) != 3:  # gets the string and splits. checking if its in time format(not really, lazy way)
+            data.pop(i)
+            continue
+        i += 1
+    return data
+
+def show_tables():
     cnn = mysql.connector.connect(port=3306, host='localhost', database='timeproject', password='kartoshka')
 
     cnn.connect()
-
-
     cursor = cnn.cursor()
-    str = []
-
-    cursor.execute('select * from data;')
-    print('Collecting Data..')
-    tim.sleep(2)
-    data = cursor.fetchall()
-
-    for item, time, stamp in data:
-        'to convert hh:mm:ss format to hh.hhh'
-        time = time.total_seconds()
-        hours = int(time // 3600)
-        minutes = int((time % 3600) // 60)
-        seconds = time % 60
-        'finish'
-
-        hours_fraction = float(hours + minutes / 60 + seconds / 3600) #type = float
-
-        item = (f'{item}', hours_fraction)# + , f'{stamp}
-        str.append(item)
-
-    "converts into float for plotext" #i tried but somehow it didnt work
-    if format == 'out':
-        items, time_values = zip(*str)
-        plt.bar(items, time_values)
-        plt.xlabel("Item")
-        plt.ylabel("Time (hours)")
-        plt.title("Time Spent on Each Item")
-
-        plt.show()
-    else:
-        items, time_values = zip(*str)
-        plx.bar(items, time_values)
-        plx.xlabel("Item")
-        plx.ylabel("Time (hours)")
-        plx.title("Time Spent on Each Item")
-
-        plx.show()
-
-    cnn.cursor().close()
+    'returns a list of all tables available; max = 2Bil'
+    cursor.execute('show tables;')
+    existing_tables = cursor.fetchall()
+    cursor.close()
     cnn.close()
-    return print('Bar chart rendered..')
+    return existing_tables
 
-
-def linear_regression():
-    print('used')
-
-
-def streaks():
-    print('used')
-
-
-
+# print(streaks())
+print(streaks())
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from importsl import data_functions
@@ -9,74 +10,73 @@ from threading import Thread
 
 cnn = mysql.connector.connect(port=3306, host='localhost', database='timeproject', password='kartoshka')
 
+date_yymmdd = datetime.datetime.today().strftime('%Y_%m_%d')
 
 '# if there is something i learnt , always use return'
 '# like return print()'
 print('STARTING...\n')
 print('CONNECTED TO SERVER')
 
-
+'do more classes and object oriented programming'
 def check_cmd(inp):
     try:
+
         if len(inp.split()) > 1 and inp.split()[0] == '/info':
             # makes sure input from user is more than 1 word, so it doesnt return an error (list index out of range)
 
             if inp.split()[1] == 'delete':
                 return print(disc.delete)
 
-            elif inp.split()[1] == 'terminate':
+            if inp.split()[1] == 'terminate':
                 return print(disc.terminate)
 
-            elif inp.split()[1] == 'streak':
+            if inp.split()[1] == 'streak':
                 return print(disc.streak)
 
-            elif inp.split()[1] == 'add_data':
+            if inp.split()[1] == 'add_data':
                 return print(disc.add_data)
 
-            elif inp.split()[1] == 'data_map':
+            if inp.split()[1] == 'data_map':
                 return print(disc.data_map)
 
-            elif inp.split()[1] == 'data_graph':
+            if inp.split()[1] == 'data_graph':
                 return print(disc.data_graph)
 
-            elif inp.split()[1] == 'linear_regression':
+            if inp.split()[1] == 'linear_regression':
                 return print(disc.linear_regression)
-            else:
-                return print('Sorry, that does not exist.')
 
         if inp.split()[0] == '/info' and len(inp.split()) == 1:
             return print(disc.info)
 
-        elif inp.split()[0] == '/cmd':
+        if inp.split()[0] == '/cmd':
             return print(disc.cmd)
 
-        elif inp.split()[0] == '/help':
+        if inp.split()[0] == '/help':
             return print(disc.help)
 
-        elif inp.split()[0] == '/add_data':
+        if inp.split()[0] == '/add_data':
             return data_functions.add_data()
 
-        elif inp.split()[0] == '/delete':
+        if inp.split()[0] == '/delete':
             try:
                 if inp.split()[1] == 'all':
 
-                    query = f'delete from data where variable=(%s)'
+                    query = f'delete from {date_yymmdd} where item=(%s) order by timestamp'
 
                     cnn.connect()
                     print(f'Deleting all {inp.split()[2]}..')
                     cnn.cursor().execute(query, (inp.split()[2],))
                     print('Deleted')
-
                     cnn.commit()
                     cnn.cursor().close()
                     cnn.close()
 
                     return None
 
-                elif inp.split()[1] == 'recent':
+                if inp.split()[1] == 'recent':
                     print(f'Deleting last entry of: {inp.split()[2]}..')
                     cnn.connect()
-                    query = 'delete from data where variable=(%s) order by timestamp desc limit 1;'
+                    query = f'delete from {date_yymmdd} where item=(%s) order by timestamp desc limit 1;'
                     cnn.cursor().execute(query, (inp.split()[2],))
                     print(f'Deleted')
                     cnn.commit()
@@ -84,15 +84,14 @@ def check_cmd(inp):
                     cnn.close()
                     return None
 
-                elif inp.split()[1] == 'last':
+                if inp.split()[1] == 'last':
                     print('Deleting..')
                     cnn.connect()
-                    cnn.cursor().execute('delete from data order by timestamp desc limit 1;')
+                    cnn.cursor().execute(f'delete from {date_yymmdd} order by timestamp desc limit 1;')
                     cnn.commit()
                     print('Deleted..')
                     cnn.cursor().close()
                     cnn.close()
-
                     return
 
 
@@ -100,16 +99,16 @@ def check_cmd(inp):
                 print(f'Error: {e}\nTry /info delete \n /delete is different from other commands.')
 
 
-        elif (inp.split()[0] == '/terminate') or (inp == 'x'):
+        if (inp.split()[0] == '/terminate') or (inp == 'x'):
             return '/terminate'
 
-        elif inp.split()[0] == '/streak':
+        if inp.split()[0] == '/streak':
             return data_functions.streaks()
 
-        elif inp.split()[0] == '/data_map':
+        if inp.split()[0] == '/data_map':
             return data_functions.data_map()
 
-        elif inp.split()[0] == '/data_graph':
+        if inp.split()[0] == '/data_graph':
 
             if (len(inp.split()) > 1):
 
@@ -120,24 +119,21 @@ def check_cmd(inp):
                     return data_functions.render_bar(format='out')
 
             return data_functions.render_bar(format='in')
-        #i might have to make a new table for each day, in a database, and compare tables each day to see if they match.
 
 
-        elif inp.split()[0] == '/linear_regression':
+        if inp.split()[0] == '/linear_regression':
             return data_functions.linear_regression()
 
-        elif inp.split()[0] == '/':
+        if inp.split()[0] == '/tables':
+            return print(data_functions.show_tables())
+
+        if inp.split()[0] == '/':
             return start_script()
 
-
-        else:
-            if len(inp.split()) > 1:
-                if inp.split()[1] == 'data_map' or 'data_graph' or 'linear_regression' or 'terminate' or 'delete' or 'add_data' or 'streak' or 'help' or 'cmd' or 'info':
-                    pass
-
-            return print(f'Sorry, the specified command does not exist.')
     except Exception as e:
-        print(f'Error: {e}')
+        print(e)
+
+
 def start_script():
     try:
         print("""
@@ -166,6 +162,7 @@ Do '/' to reset
         return print(f'An error encountered: {e}', f'\nReason: {inp}')
 '^ This is most likely complete ^'
 
+
 def get_input():
     try:
         user_input = input('\n>')
@@ -191,5 +188,3 @@ while True:
 
 
 print('CLOSING CONNECTION...')
-cnn.cursor().close()
-cnn.close()
